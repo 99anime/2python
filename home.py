e8c44bc33493de7202ba7ebe0c555eab8ed4b095
99anime <89612530+99anime@users.noreply.github.com>
2021-09-01 14:12:45 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,10 @@
 from flask import Flask,render_template
 app=Flask(__name__)
+@app.route("home")
+def home():
+ return render_template("home.html")
 @app.route("/anime")
 def anime():
- return render_template("anime.html");
+ return render_template("anime.html")
  if __name__=='main':
   app.run(debug=True)
